06e8913347fab61ec09a3ea4fa6c0488b36a6de6
George Sykes <gsykes537@gmail.com>
2025-07-02 20:00:26 +0100

    feat: read from a GPIO pin

--- tomato_pi.cpp
+++ tomato_pi.cpp
@@ -1,7 +1,21 @@
 #include <fmt/core.h>
+#include <wiringPi.h>
+#include <stdio.h>
 
 int main()
 {
-    fmt::print("Hello World!\n");
+    if (wiringPiSetup() == -1)
+    {
+        fmt::print("Error: wiring pi not set up\n");
+        return 1;
+    }
+
+    pinMode(0, OUTPUT); // aka BCM_GPIO pin 17
+    while (true)
+    {
+        auto value = digitalRead(0); // On
+        fmt::print("LED is {}\n", value ? "ON" : "OFF");
+        delay(500); // mS
+    }
     return 0;
 }
